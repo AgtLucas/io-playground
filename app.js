0e5131a39e7f30d0a984318acf3affe504f4cbcb
Lucas <lucas@agtlucas.com>
2015-01-24 22:01:44 -0200

    Update app.js

--- app.js
+++ app.js
@@ -8,4 +8,15 @@ app.use(function *(next) {
   this.set('X-Response-Time', ms + 'ms');
 });
 
+app.use(function *(next) {
+  var start = new Date;
+  yield next;
+  var ms = new Date - start;
+  console.log('%s %s - %s', this.method, this.url, ms);
+});
+
+app.use(function *() {
+  this.body = 'Yo, io.js!';
+});
+
 app.listen(3000);
